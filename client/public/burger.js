eaf84d9a5f3794ec91c79b3d4c17bdf9ca739d36
Angy <yaojeanangoa@gmail.com>
2026-09-03 18:19:33 +0000

    fixed: error corrigée

--- client/public/burger.js
+++ client/public/burger.js
@@ -1,16 +1,10 @@
-// Récupérer les éléments du DOM
 const burgerToggle = document.getElementById('burger-toggle');
-const navMenu = document.querySelector('.nav-menu'); // Remplacez par la classe de votre menu
+const navMenu = document.querySelector('.burger nav');
 
-// Écouter le changement d'état de la checkbox
 burgerToggle.addEventListener('change', () => {
     if (burgerToggle.checked) {
-        // Actions à faire quand le menu s'ouvre
-        navMenu.classList.add('active');
-        console.log("Menu ouvert");
+        navMenu.style.display = 'block';
     } else {
-        // Actions à faire quand le menu se ferme
-        navMenu.classList.remove('active');
-        console.log("Menu fermé");
+        navMenu.style.display = 'none';
     }
 });
